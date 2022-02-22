37f742a5f6849e1f63e94707c2cc4e10167f0827
Selim Reza <mdselimreza.buft@gmail.com>
2022-02-22 17:49:08 +0600

    increase/decrease working

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 // common variable::
 const plusPhoneBtn = document.getElementById('phone-plus');
 const minusPhoneBtn = document.getElementById('phone-minus');
-const
+const plusCaseBtn = document.getElementById('case-plus');
+const minusCaseBtn = document.getElementById('case-minus');
 // 
 
 
 
 // function for input field::
+function getInputField(inputID, isAdd) {
+    const inputValue = parseInt(document.getElementById(inputID).value);
+    if (isAdd == true) {
+        document.getElementById(inputID).value = inputValue + 1;
+    } else {
+        document.getElementById(inputID).value = inputValue - 1;
+    }
 
-
-
-
-
-
+}
 // output command::
+plusPhoneBtn.addEventListener('click', function() {
+    getInputField('phone-number', true);
+});
+minusPhoneBtn.addEventListener('click', function() {
+    getInputField('phone-number', false);
+});
+plusCaseBtn.addEventListener('click', function() {
+    getInputField('case-number', true);
+});
+minusCaseBtn.addEventListener('click', function() {
+    getInputField('case-number', false);
+});
