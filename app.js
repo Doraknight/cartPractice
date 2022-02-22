e10ce901ead7d9285f7d816e9ba55295cd5e8819
Selim Reza <mdselimreza.buft@gmail.com>
2022-02-22 17:36:26 +0600

    adding to git

---
+++ app.js
@@ -0,0 +1,16 @@
+// common variable::
+const plusPhoneBtn = document.getElementById('phone-plus');
+const minusPhoneBtn = document.getElementById('phone-minus');
+const
+// 
+
+
+
+// function for input field::
+
+
+
+
+
+
+// output command::
