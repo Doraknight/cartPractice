3558b8fd80993d2544ea5e0ef4314e57edebf860
Selim Reza <mdselimreza.buft@gmail.com>
2022-02-23 03:05:50 +0600

    all set

--- app.js
+++ app.js
@@ -4,30 +4,53 @@ const minusPhoneBtn = document.getElementById('phone-minus');
 const plusCaseBtn = document.getElementById('case-plus');
 const minusCaseBtn = document.getElementById('case-minus');
 // 
-
-
-
-// function for input field::
-function getInputField(inputID, isAdd) {
-    const inputValue = document.getElementById(inputID);
-    let productAmount = inputValue.value;
+function getInputField(product, price, isAdd) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if (isAdd == true) {
-        productAmount = parseInt(productAmount) + 1;
-    } else if (inputValue > 0) {
-        productAmount = parseInt(productAmount) - 1;
+        productInput.value = parseInt(productNumber) + 1;
+    } else if (productNumber > 0) {
+        productInput.value = parseInt(productNumber) - 1;
     }
+    const productTotalNumber = productInput.value;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productTotalNumber * price;
+    // 
+    updateInputFieldAmount();
 
 }
+// 
+function fieldCall(product) {
+    const inputAmount = document.getElementById(product + '-number');
+    const inputTotalAmount = parseInt(inputAmount.value);
+    return inputTotalAmount;
+}
+
+function updateInputFieldAmount() {
+    const phoneTotalValue = fieldCall('phone') * 1219;
+    const caseTotalValue = fieldCall('case') * 59;
+    const subTotal = phoneTotalValue + caseTotalValue;
+    const subTotalInput = document.getElementById('sub-total');
+    subTotalInput.innerText = subTotal;
+    const taxId = document.getElementById('tax-amount');
+    const taxAmount = subTotal * 0.03;
+    taxId.innerText = taxAmount.toFixed(2);
+    const totalId = document.getElementById('total-price');
+    const totalPrice = subTotal + taxAmount;
+    totalId.innerText = totalPrice.toFixed(2);
+}
+
+
 // output command::
 plusPhoneBtn.addEventListener('click', function() {
-    getInputField('phone-number', true);
+    getInputField("phone", 1219, true);
 });
 minusPhoneBtn.addEventListener('click', function() {
-    getInputField('phone-number', false);
+    getInputField('phone', 1219, false);
 });
 plusCaseBtn.addEventListener('click', function() {
-    getInputField('case-number', true);
+    getInputField('case', 59, true);
 });
 minusCaseBtn.addEventListener('click', function() {
-    getInputField('case-number', false);
+    getInputField('case', 59, false);
 });
